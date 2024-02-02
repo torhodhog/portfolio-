7293c4f69f0eacf8d41e8013ecc8209bbac54a83
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-02 11:40:13 +0100

    contact and project folder/files

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,10 @@
+// link (next js)
 import Link from 'next/link';
+
+
+//next hooks
+import { usePathname } from 'next/navigation';
+
 
 const links = [
    {path: '/', name: 'home'},
@@ -7,10 +13,11 @@ const links = [
 ]
 
 const Nav = ({containerStyles, linkStyles, underlineStyles}) => {
+   const path = usePathname();
   return (
     <nav className={`${containerStyles}`}>
       {links.map((link, index)=>{
-         return <Link href={link.path}>{link.name}</Link>
+         return <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>{link.name}</Link>
       })}
     </nav>
   )
