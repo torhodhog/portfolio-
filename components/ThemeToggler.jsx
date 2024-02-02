65ced6d5a48f819a38f454dbb67a6312fc2ff124
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-02 09:19:30 +0100

    First commit, problems with radic-ui/react-icons

---
+++ components/ThemeToggler.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Button } from "./ui/button";
+import {MoonIcon, SunIcon} from "@radic-ui/react-icons";
+import { useTheme } from "next-themes";
+
+const ThemeToggler = () => {
+  const {theme, setTheme} = useTheme();
+   return (
+    <div>
+      <Button>
+         <SunIcon />
+         <MoonIcon />
+      </Button>
+    </div>
+  )
+}
+
+export default ThemeToggler
