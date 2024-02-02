a5eafa7792b4ca7ba7ee00c40f24acabbf7c660e
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-02 23:19:46 +0100

    Alt oppe og går, jobber med hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,13 +16,17 @@ import Socials from "./Socials";
 
 const Hero = () => {
   return (
-   <section className="">
+   <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
          <div className="container mx-auto">
             <div className="flex justify-between gap-x-8">
                {/* text */}
-               <div>text</div>
+               <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+               <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]" >Web Developer</div>
+               <h1 className="h1">Hello, my name is Torgeir</h1>
+               <p>Brief description with insights into myself, my covational jurney, and what I engage in professionally</p>
+               </div>
                {  /* image */}
-               <div>image</div>   
+               <div className="hidden xl:flex relative">image</div>
             </div>
             {/* icon */}
             <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
--- tailwind.config.js
+++ tailwind.config.js
@@ -71,6 +71,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        her: 'url(/hero/hero-bg.png)',
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
