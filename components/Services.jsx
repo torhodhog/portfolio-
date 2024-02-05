3cb21d0145de61f0b33a4dc520e4df12bcc25290
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-05 20:23:05 +0100

    services done

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,38 +1,58 @@
-import { GanttChartSquare, Blocks, Gem} from "lucide-react";
-import {Card, CardContent, CardHeader, CardTitle} from "./ui/card";
+import { GanttChartSquare, Blocks, Gem } from "lucide-react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 
 const servicesData = [
-   {
-      icon: <GanttChartSquare size={72} strokeWidth={0.8}/>,
-      title:'Web Design',
-      description: 'Building websites and web applications that are fast, secure, and easy to use.',
-   },
-   {
-      icon: <Blocks size={72} strokeWidth={0.8}/>,
-      title:'Web Development',
-      description: 'Building websites and web applications that are fast, secure, and easy to use.',
-   },
-   {
-      icon: <Gem size={72} strokeWidth={0.8}/>,
-      title:'App Development',
-      description: 'Building websites and web applications that are fast, secure, and easy to use. Learn more about my app development services.',
-   },
-]
+  {
+    icon: <GanttChartSquare size={72} strokeWidth={0.8} />,
+    title: "Web Design",
+    description:
+      "Building websites and web applications that are fast, secure, and easy to use.",
+  },
+  {
+    icon: <Blocks size={72} strokeWidth={0.8} />,
+    title: "Web Development",
+    description:
+      "Building websites and web applications that are fast, secure, and easy to use.",
+  },
+  {
+    icon: <Gem size={72} strokeWidth={0.8} />,
+    title: "App Development",
+    description:
+      "Building websites and web applications that are fast, secure, and easy to use. Learn more about my app development services.",
+  },
+];
 
 const Services = () => {
   return (
     <section>
       <div className="container mx-auto">
-         <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto">My Services</h2>
-         {/* GRID ITEMS */}
-         <div>
-            {servicesData.map((item, index) => {
-               return <Card key={index}>Card</Card>
-            })}
-         </div>
+        <h2 className="section-title mb-12 xl:mb-24 text-center mx-auto justify-center items-center relative">
+          My Services
+        </h2>
+        {/* GRID ITEMS */}
+        <div className=" grid xl:grid-cols-3 justify-center gap-y-12 cl:gap-y-24 xl:gap-x-8">
+          {servicesData.map((item, index) => {
+            return (
+              <Card
+                className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                key={index}
+              >
+                <CardHeader className="text-primary absolute -top-[60px]">
+                  <div className="w-[140px] h-[80px] bg-white dark:bg-background flex justify-center items-center">
+                    {item.icon}
+                  </div>
+                </CardHeader>
+                <CardContent className='text-center'>
+                  <CardTitle className='mb-4'>{item.title}</CardTitle>
+                  <CardDescription className='text-lg'>{item.description}</CardDescription>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
