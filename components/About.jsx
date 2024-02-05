cade35f620bcabe2bd7509af65c43c1c44faf9b5
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-05 13:58:53 +0100

    experience done

--- components/About.jsx
+++ components/About.jsx
@@ -192,9 +192,9 @@ const About = () => {
                       Min Reise
                     </h3>
                     {/* experience and education wrapper */}
-                    <div>
+                    <div className="grid md:grid-cols-2 gap-y-8">
                       {/* experience */}
-                      <div>
+                      <div className="flex flex-col gap-y-6">
                         <div className="flex gap-x-4 items-center text-[22px] text-primary">
                           <Briefcase size={20} />
                           <h4 className="capitalize font-medium">
@@ -208,7 +208,7 @@ const About = () => {
                               const { company, role, years } = item;
                               return (
                                 <div
-                                  className="flex gap-x-8 group bg-yellow-100"
+                                  className="flex gap-x-8 group"
                                   key={index}
                                 >
                                   <div className="h-[84px] w-[1px] bg-border relative ml-2">
@@ -232,7 +232,44 @@ const About = () => {
                         </div>
                       </div>
                       {/* education */}
-                      <div>education</div>
+                      
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <GraduationCap size={20} />
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "education").title}
+                          </h4>
+                        </div>
+                        {/* experience list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "education").data.map(
+                            (item, index) => {
+                              const { university, qualification, years } = item;
+                              return (
+                                <div
+                                  className="flex gap-x-8 group"
+                                  key={index}
+                                >
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {university}
+                                    </div>
+                                    <div className="text-lg leadning-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </TabsContent>
