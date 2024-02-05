6c9ce0e1ac232ae05ae0a9d7d5eea630ab1c942e
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-05 13:11:35 +0100

    about - qualification

--- components/About.jsx
+++ components/About.jsx
@@ -1,155 +1,249 @@
-import DevImg from './DevImg';
-import Image from 'next/image';
-import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/ui/tabs';
+import DevImg from "./DevImg";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import {User2, MailIcon, HomeIcon, PhoneCall, GraduationCap, Calendar, Briefcase} from 'lucide-react';
-
+import {
+  User2,
+  MailIcon,
+  HomeIcon,
+  PhoneCall,
+  GraduationCap,
+  Calendar,
+  Briefcase,
+} from "lucide-react";
 
 const infoData = [
-   {
-      icon: <User2 size={20}/>,
-      text: 'Torgeir Hodne-Høgheim',
-   },
-   {
-      icon: <PhoneCall size={20}/>,
-      text: '+47 92056241',
-   },
-   {
-      icon: <MailIcon size={20}/>,
-      text: 'hogheim@gmail.com',
-   },
-   {
-      icon: <Calendar size={20}/>,
-      text: 'Born on 20 Jul, 1990',
-   },
-   {
-      icon: <GraduationCap size={20}/>,
-      text: 'Front End Developer',
-   },
-   {
-      icon: <HomeIcon size={20}/>,
-      text: 'Helgeplasset 114, 5148 Fyllingsdalen',
-   },
-]
+  {
+    icon: <User2 size={20} />,
+    text: "Torgeir Hodne-Høgheim",
+  },
+  {
+    icon: <PhoneCall size={20} />,
+    text: "+47 92056241",
+  },
+  {
+    icon: <MailIcon size={20} />,
+    text: "hogheim@gmail.com",
+  },
+  {
+    icon: <Calendar size={20} />,
+    text: "Born on 20 Jul, 1990",
+  },
+  {
+    icon: <GraduationCap size={20} />,
+    text: "Front End Developer",
+  },
+  {
+    icon: <HomeIcon size={20} />,
+    text: "Helgeplasset 114, 5148 Fyllingsdalen",
+  },
+];
 
 const qualificationData = [
-   {
-      title:  'education',
-      dat: [
-         {
-            university: 'Høgskolen på Vestlandet',
-            qualification: 'Bachelor, lærer, GLU 1-7',
-            years: '2010-2014',
-         },
-         {
-            university: 'Noroff institute of technology and Media',
-            qualification: 'Back End Developer',
-            years: '2023-2024'
-         },
-         {
-            university: 'Gokstad Akademiet',
-            qualification: 'Front End Developer',
-            years: '2023-2025'
-         },
-      ]
-   },
-   {
-      title:  'experience',
-      dat: [
-         {
-            company: 'Bergen Kommune',
-            qualification: 'Kontaklærer, 1-7. klasse',
-            years: '2015-2023',
-         },
-       
-      ]
-   },
-]
+  {
+    title: "education",
+    data: [
+      {
+        university: "Høgskolen på Vestlandet",
+        qualification: "Bachelor, lærer, GLU 1-7",
+        years: "2010-2014",
+      },
+      {
+        university: "Noroff institute of technology and Media",
+        qualification: "Back End Developer",
+        years: "2023-2024",
+      },
+      {
+        university: "Gokstad Akademiet",
+        qualification: "Front End Developer",
+        years: "2023-2025",
+      },
+    ],
+  },
+  {
+    title: "experience",
+    data: [
+      {
+        company: "Bergen Kommune",
+        role: "Kontaklærer, 1-7. klasse",
+        years: "2015-2023",
+      },
+    ],
+  },
+];
 
-const skillData =[
-   {
-      title: 'skills',
-      data: [
-         {
-            name: 'HTML, CSS',
-         },
-         {
-            name: 'JavaScript',
-         },
-         {
-            name: 'Node.js',
-         },
-         {
-            name: 'Express.js',
-         },
-         {
-            name: 'Next.js',
-         },
-         {
-            name: 'React.js',
-         },
-      ]
-   },
-   {
-      title: 'tools',
-      data: [
-         {
-            imgPath: '/about/vscode.svg',
-         },
-         {
-            imgPath: '/about/figma.svg',
-         },
-         {
-            imgPath: '/about/notion.svg',
-         },
-         {
-            imgPath: '/about/wordpress.svg',
-         },
-      ]
-   },
-]
+const skillData = [
+  {
+    title: "skills",
+    data: [
+      {
+        name: "HTML, CSS",
+      },
+      {
+        name: "JavaScript",
+      },
+      {
+        name: "Node.js",
+      },
+      {
+        name: "Express.js",
+      },
+      {
+        name: "Next.js",
+      },
+      {
+        name: "React.js",
+      },
+    ],
+  },
+  {
+    title: "tools",
+    data: [
+      {
+        imgPath: "/about/vscode.svg",
+      },
+      {
+        imgPath: "/about/figma.svg",
+      },
+      {
+        imgPath: "/about/notion.svg",
+      },
+      {
+        imgPath: "/about/wordpress.svg",
+      },
+    ],
+  },
+];
 
 const About = () => {
- 
-   const getData = (arr, title) => {
-      return arr.find((item)=> item.title === title)
-   };
- return (
-    <section className='xl:h-[860px] pb-12 xl:py-24'>
+  const getData = (arr, title) => {
+    return arr.find((item) => item.title === title);
+  };
+  return (
+    <section className="xl:h-[860px] pb-12 xl:py-24">
       <div className="container mx-auto">
-         <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>About Me</h2>
-         <div className='flex flex-col xl:flex-row'>
-            {/* image */}
-            <div className='hidden xl:flex flex-1 relative'>
-               <DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-ro-repeat relative' imgSrc='/hero/developer.png'/>
-            </div>
-            { /* tabs */}
-            <div className='flex-1'>
-               <Tabs defaultValue='personal'>
-                  <TabsList className='w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none'> 
-                     <TabsTrigger className='w-[162px] xl:w-auto' value='personal'>Info</TabsTrigger>
-                     <TabsTrigger className='w-[162px] xl:w-auto' value='qualifications'>Qualification</TabsTrigger>
-                     <TabsTrigger className='w-[162px] xl:w-auto' value='skills'>Skills</TabsTrigger>
-                  </TabsList>
-                  { /* tabs content */}
-                  <div className='text-lg mt-12 xl:mt-8'>
-                     {/* personal */}
-                     <TabsContent value='personal'>
-                        <div>
-                           <h3 className='h3 mb-4'>Unmatched Service Quality for Over 10 years</h3>
-                           <p className='subtitle max-w-xl max-auto xl:mx-0'>I specialize in crafting intuitive websites with cutting-edge technology, delivering dynamic and engaging user experiences.</p>
-                        </div>
-                     </TabsContent>
-                     <TabsContent value='qualifications'>qualications info</TabsContent>
-                     <TabsContent value='skills'>skills info</TabsContent>
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          About Me
+        </h2>
+        <div className="flex flex-col xl:flex-row">
+          {/* image */}
+          <div className="hidden xl:flex flex-1 relative">
+            <DevImg
+              containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-ro-repeat relative"
+              imgSrc="/hero/developer.png"
+            />
+          </div>
+          {/* tabs */}
+          <div className="flex-1">
+            <Tabs defaultValue="personal">
+              <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
+                <TabsTrigger className="w-[162px] xl:w-auto" value="personal">
+                  Info
+                </TabsTrigger>
+                <TabsTrigger
+                  className="w-[162px] xl:w-auto"
+                  value="qualifications"
+                >
+                  Qualification
+                </TabsTrigger>
+                <TabsTrigger className="w-[162px] xl:w-auto" value="skills">
+                  Skills
+                </TabsTrigger>
+              </TabsList>
+              {/* tabs content */}
+              <div className="text-lg mt-12 xl:mt-8">
+                {/* personal */}
+                <TabsContent value="personal">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-4">
+                      Unmatched Service Quality for Over 10 years
+                    </h3>
+                    <p className="subtitle max-w-xl max-auto xl:mx-0">
+                      I specialize in crafting intuitive websites with
+                      cutting-edge technology, delivering dynamic and engaging
+                      user experiences.
+                    </p>
+                    {/* info */}
+                    <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                      {infoData.map((item, index) => {
+                        return (
+                          <div
+                            className="flex items-center gap-x-4 mx-auto xl:mx-0"
+                            key={index}
+                          >
+                            <div className="text-primary">{item.icon}</div>
+                            <div>{item.text}</div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    {/* languages */}
+                    <div className="flex flex-col gap-y-2">
+                      <div className="text-primary">Language Skill</div>
+                      <div className="border-b border-border"></div>
+                      <div>Norgewian, Enligsh</div>
+                    </div>
                   </div>
-               </Tabs>
-            </div>
-         </div>
+                </TabsContent>
+                {/* qualifications */}
+                <TabsContent value="qualifications">
+                  <div>
+                    <h3 className="h3 mb-8 text-center xl:text-left">
+                      Min Reise
+                    </h3>
+                    {/* experience and education wrapper */}
+                    <div>
+                      {/* experience */}
+                      <div>
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <Briefcase size={20} />
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "experience").title}
+                          </h4>
+                        </div>
+                        {/* experience list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "experience").data.map(
+                            (item, index) => {
+                              const { company, role, years } = item;
+                              return (
+                                <div
+                                  className="flex gap-x-8 group bg-yellow-100"
+                                  key={index}
+                                >
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {company}
+                                    </div>
+                                    <div className="text-lg leadning-none text-muted-foreground mb-4">
+                                      {role}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
+                      {/* education */}
+                      <div>education</div>
+                    </div>
+                  </div>
+                </TabsContent>
+                <TabsContent value="skills">skills info</TabsContent>
+              </div>
+            </Tabs>
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
-
+export default About;
