0714dc6f2bbc3f4dc40a9bcd0a3f41fca449b6fb
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-05 17:35:15 +0100

    about image

--- components/About.jsx
+++ components/About.jsx
@@ -136,7 +136,7 @@ const About = () => {
           <div className="hidden xl:flex flex-1 relative">
             <DevImg
               containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-ro-repeat relative"
-              imgSrc="/hero/developer.png"
+              imgSrc="/hero/developer2.png"
             />
           </div>
           {/* tabs */}
